62b51123c9800cbd0a91046f793d0265cabfe41b
SleekPanther <macrobyte2@gmail.com>
2017-01-23 09:15:13 -0500

    initial idea works (with function)

---
+++ LoopPerformance.java
@@ -0,0 +1,30 @@
+public class LoopPerformance {
+	public static void main(String[] args) {
+		LoopPerformance.test2Loops(10000000);
+	}
+	
+	public static void test2Loops(long upperBound){
+		long dummyNum= 0;
+		long start = System.nanoTime();
+		for(long i=1; i<= upperBound; i++){
+			dummyNum=i;
+		}
+		long end = System.nanoTime();
+		long postIncrementTime=end-start;
+		
+		System.out.printf(dummyNum +" terations with i++ took: %,35d\n", postIncrementTime);
+		
+		
+		dummyNum= 0;
+		start = System.nanoTime();
+		for(long i=1; i<= upperBound; ++i){
+			dummyNum=i;
+		}
+		end = System.nanoTime();
+		long preIncrementTime=end-start;
+		
+		System.out.printf(dummyNum +" terations with ++i took: %,35d\n", preIncrementTime);
+		
+		System.out.printf("Difference (post - pre): %,d  nanoseconds\n", (postIncrementTime-preIncrementTime));
+	}
+}
